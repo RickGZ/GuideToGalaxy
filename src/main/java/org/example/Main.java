bf507fab3888d2d1708197f61120402c7ef2a5ce
RickGZ <rickgz66@gmail.com>
2022-10-31 15:35:18 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        // Get data using BufferedReader
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        // Read Data
+        String input = null;
+        try {
+            input = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Print result
+        System.out.println(input);
+    }
+}
