318f81645c9867261a5bae7877e134211714898f
RickGZ <rickgz66@gmail.com>
2022-11-01 22:02:44 +0100

    -

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,5 +1,8 @@
 package org.main;
 
+import org.interpreter.IInterpreter;
+import org.interpreter.Interpreter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,18 +10,18 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         // Get data using BufferedReader
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+        IInterpreter interpreter = new Interpreter();
 
         // Read Data
-        String input = null;
-        try {
-            input = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while (true) {
+            String result = null;
+            try {
+                result = interpreter.handleInput(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(result);
         }
-
-        // Print result
-        System.out.println(input);
     }
 }
